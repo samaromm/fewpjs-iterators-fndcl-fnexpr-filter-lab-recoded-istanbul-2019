7c6a306b8359dd2d9b603aee42a72ea05ab04a7c
Samar Omar <samaromm98@gmail.com>
2020-01-30 11:14:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(arr, name){
+   return arr.filter(function(e){
+   return e.toLowerCase()==name.toLowerCase()
+})}
+
+function fuzzyMatch(arr, letters){
+   return arr.filter(function(e){
+   return e.toLowerCase().substr(0,letters.length)===letters.toLowerCase()
+})}
+
+function matchName(arr, name){
+  return arr.filter(function (e){
+    return e.name.toLowerCase()==name.toLowerCase()
+  })
+}
