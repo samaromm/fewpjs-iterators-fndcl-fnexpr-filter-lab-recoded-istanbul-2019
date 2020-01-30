40167a07dfa2e8aeb8e1f05c7f66de8d1a6b9914
Samar Omar <samaromm98@gmail.com>
2020-01-30 11:12:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function findMatching(arr, name){
 
 function fuzzyMatch(arr, letters){
    return arr.filter(function(e){
-   return e.toLowerCase().substr(0,letter.length)===name.toLowerCase()
+   return e.toLowerCase().substr(0,letters.length)===name.toLowerCase()
 })}
